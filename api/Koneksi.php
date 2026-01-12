41441993e5652d7705e725c94ed55e5ff6d55f98
bangadjie <masadjie927@gmail.com>
2026-01-12 20:12:49 +0700

    koneksi php supaya bisa di buka di laragon dan vps

--- api/Koneksi.php
+++ api/Koneksi.php
@@ -1,7 +1,15 @@
 <?php
-$host = "binabangsa-db";
+// Cek apakah script berjalan di Docker atau Laragon
+if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1' || $_SERVER['HTTP_HOST'] == 'localhost' || strpos($_SERVER['HTTP_HOST'], '.test') !== false) {
+    // Settingan untuk LARAGON (Lokal)
+    $host = "localhost";
+} else {
+    // Settingan untuk DOCKER (VPS)
+    $host = "binabangsa-db";
+}
+
 $user = "root";
-$pass = "root";
+$pass = ""; // Di Laragon biasanya kosong, di Docker nanti disesuaikan
 $db   = "binabangsa";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
